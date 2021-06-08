92bb06583b615b63d74a079af52ab34c356f7b18
Alex Couture-Beil <alex@earthly.dev>
2021-06-08 14:43:30 -0700

    wip

--- send.go
+++ send.go
@@ -2,6 +2,7 @@ package fsutil
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"os"
 	"sync"
@@ -152,7 +153,7 @@ func (s *sender) sendFile(h *sendHandle) error {
 func (s *sender) walk(ctx context.Context) error {
 	var i uint32 = 0
 	err := s.fs.Walk(ctx, func(path string, fi os.FileInfo, err error) error {
-		//fmt.Printf("ACB walk %q\n", path)
+		fmt.Printf("ACB walk %q\n", path)
 		if err != nil {
 			return err
 		}
